d9e2e24d4c5019f7f1b8156d87709b98c45a200a
João Victor Nascimento Rueda <69706617+johnnyvnr@users.noreply.github.com>
2021-03-06 17:10:28 -0300

    alteraçoesnofa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 function scrollToBox() {
     document.querySelector('.paddbox').scrollIntoView({behavior: 'smooth'});
   }
+  function scrollToBoxTwo() {
+    document.querySelector('.paddboxtwo').scrollIntoView({behavior: 'smooth'});
+  }
